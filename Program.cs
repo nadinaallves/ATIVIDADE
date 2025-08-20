c391669abbd21fe32d0b4ebf8a47e1bc72062c1f
nadinaallves <nadinaallves@gmail.com>
2025-08-19 21:28:45 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ATIVIDADE_DE_PORTIFÓLIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nome1, nome2, nome3, nome4, auxiliar;
+
+            Console.WriteLine("Digite o nome1 ");
+            nome1 = Console.ReadLine();
+
+            Console.WriteLine("Digite o nome2 ");
+            nome2 = Console.ReadLine();
+
+            Console.WriteLine("Digite o nome3 ");
+            nome3 = Console.ReadLine();
+
+            Console.WriteLine("Digite o nome4 ");
+            nome4 = Console.ReadLine();
+
+            //mecanismo para inverter nomes
+
+            auxiliar = nome1;
+            nome1 = nome4;
+
+            nome4 = auxiliar;
+
+            auxiliar = nome2;
+            nome2 = nome3;
+
+            Console.WriteLine("Nomes inseridos na sequência invertida");
+            Console.WriteLine(nome1);
+            Console.WriteLine(nome2);
+            Console.WriteLine(nome3);
+            Console.WriteLine(nome4);
+            Console.ReadKey();
+        }
+    }
+}
